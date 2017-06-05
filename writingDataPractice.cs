45999d7da3c2d355a08ac5846d7a408816101ca8
sziccardi <sziccardi@lclark.edu>
2017-06-05 15:26:04 -0700

    Code for writing data to a text file

---
+++ writingDataPractice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class writingDataPractice : Redirector
+{
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void ApplyRedirection()
+    {
+
+        string path = "Assets/Resources/test.txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Test");
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset) Resources.Load("test");
+
+        //Print the text from the file
+        //Debug.Log(asset.text);
+
+
+    }
+}
