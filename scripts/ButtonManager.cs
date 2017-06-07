24abf21d435ea02ae5af38fac1a422084c2d6a6a
Julio Medina <jamedina@hmc.edu>
2017-06-07 14:31:47 -0700

    Refactor ButtonManager.cs

--- scripts/ButtonManager.cs
+++ scripts/ButtonManager.cs
@@ -9,11 +9,8 @@ public class ButtonManager : MonoBehaviour {
     //We want button presses to be triggered after a short amount of time
     //instead of immediately, for the sake of the user. So we use a counter
     //to increment time.
-    private int topCounter = 0;
-    private int bottomCounter = 0;
-
-    public AudioClip confirm;
-
+    private int Counter = 0;
+    public AudioClip soundFile;
 
     void writeToFile(string text)
     {
@@ -24,30 +21,15 @@ public class ButtonManager : MonoBehaviour {
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.name == "Top")
+        ++Counter;
+        if (Counter >= 20)
         {
-            ++topCounter;
-            if (topCounter >= 15)
-            {
-                GetComponent<AudioSource>().PlayOneShot(confirm);
-                Debug.Log("I Want MORE");
-                writeToFile("I Wanted MORE");
-                topCounter = 0;
-                SceneManager.LoadScene("Redirected walking scene");
-            }
-
+            GetComponent<AudioSource>().PlayOneShot(soundFile);
+            Debug.Log(this.gameObject.name);
+            writeToFile(this.gameObject.name);
+            Counter = 0;
+            SceneManager.LoadScene("Redirected walking scene");
         }
-        else if (col.gameObject.name == "Bottom")
-        {
-            ++bottomCounter;
-            if (bottomCounter >= 15)
-            {
-                GetComponent<AudioSource>().PlayOneShot(confirm);
-                Debug.Log("I Want LESS");
-                writeToFile("I Wanted LESS");
-                bottomCounter = 0;
-                SceneManager.LoadScene("Redirected walking scene");
-            }
-        }   
-    }
+     }
 }
+
