2822aa5ea12974d3a6b9b1ccedd0c719eadc1d41
Julio Medina <jamedina@hmc.edu>
2017-06-12 16:18:30 -0700

    remove unused refrences

--- scripts/Pest.cs
+++ scripts/Pest.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Redirection;
@@ -8,28 +8,16 @@ using System.Linq;
 
 public class RotationTest : Redirector
 {
-    public static float minGain = -.99f;
-    public static float maxGain = .49f;
-    static float currentGain = .5f;
-
-    public static uint maxTrials;
-    static uint trialNum;
-
     public GameObject yesButton;
     public GameObject noButton;
 
-    //Staircase specific variables
-    static float adjustmentAmount = 0.5f;
-    static bool lowering = true;
-    /////////////////////////////
-
-    //Constant Stimuli specific variables
+    //public static float minGain = -.99f;
+    //public static float maxGain = .49f;
+    static float currentGain = .5f;
+    public static uint maxTrials;
     static int numLevels;
-    ////////////////////////////
 
-    //PEST specific variables
-
-    ////////////////////////////
+    
 
     private string getLastLine(string path)
     {
@@ -48,62 +36,44 @@ public class RotationTest : Redirector
         writer.Close();
     }
 
-    /// <summary>
-    /// A set of equally spaced levels of the stimulus intensities is chosen
-    /// (usually 5-9). Each level is repeated large number of times. The
-    /// subject is asked to report whether the presented stimulus can be
-    /// detected(when AL is measured), or whether the intensity of the
-    /// presented test stimulus is greater than that of the reference stimulus
-    /// (when DL is measured).
-    /// </summary>
-    public void constantStimuli()
-    {
-        string lastLine = getLastLine("Assets/test.txt");
-        //TODO: Print old gain and whether person recognized it into separate file
 
-        float[] possibleLevels = new float[numLevels];
-        for (int i = 0; i < numLevels; ++i)
-        {
-            possibleLevels[i] = minGain + (maxGain - minGain) * (i / numLevels);
-        }
+    
 
-
-
-
-    }
-
-    public void PEST()
+    public void pest()
     {
 
         //INITIALIZATION//
-        //max stim value that it is surely detected
-        float stimRange = 2.0f; 
-        //each iteration of 1 jumps a certain step size which = ( stimRange / numLevels )
+        //Easily detectable maximum stimulus value
+        float stimRange = 2.0f;
+        //each iteration of I jumps ( stimRange / numLevels )
         int numTrials = 4;
 
-        float[] prob = new float[numLevels * 2]; //cumulative probability that the threshold is at eaech of the possible values of the independent variable based on user responses
+        //cumulative probability that threshold is at each of the possible values of the independent variable based on user responses
+        float[] prob = new float[numLevels * 2]; 
 
-        float[] plgit = new float[numLevels * 2]; //psychometric function. probablity of a positive response
-        float[] mlgit = new float[numLevels * 2]; //psychometric function. probability of a negative response
+        float[] plgit = new float[numLevels * 2]; //probablity of a positive response
+        float[] mlgit = new float[numLevels * 2]; //probability of a negative response
 
-        float std = stimRange / 5; //estimate slope of psychometric function
+        //estimate slope of psychometric function
+        float std = stimRange / 5;
 
-        for ( int i = 0; i < 2*numLevels; i++)
+        for (int i = 0; i < 2 * numLevels; ++i)
         {
             prob[i] = 0;
-            float lgit = 1 / (1 + Mathf.Exp((stimRange - (i*stimRange/numLevels)  ) / std)); //(i*stimRange/numLevels) is the iteration jump for stimulus values for each i
+            float lgit = 1 / (1 + Mathf.Exp((stimRange - (i * stimRange / numLevels)) / std)); //(i*stimRange/numLevels) is the iteration jump for stimulus values for each i
             plgit[i] = Mathf.Log(lgit);
             mlgit[i] = Mathf.Log(1 - lgit);
         }
+    
+        //Initialize response and stimulus level
+        int response = -1; 
+        float stimLevel = stimRange; 
 
-        int response = -1; // initialize as a negative respose 
-        float stimLevel = stimRange; // initialize 
-
-        //SUBRUITINE
-        float max = -1000f;
+        //Subroutine
+        float max = float.MinValue;
         int p1 = numLevels, p2 = numLevels; //place where the max(s) is stored
 
-        for (int j = 0; j < numLevels; j++)
+        for (int j = 0; j < numLevels; ++j)
         {
             int iterator = (int)(stimLevel * numLevels / stimRange);
             if (response == 1)
@@ -135,14 +105,14 @@ public class RotationTest : Redirector
         ////////////////////
 
         //MAIN  PROGRAM
-        for ( int i = 0; i < numTrials; i++)
+        for (int i = 0; i < numTrials; ++i)
         {
             //SUBRUITINE
             max = -1000f;
             p1 = numLevels;
             p2 = numLevels; //place where the max(s) is stored
 
-            for (int j = 0; j < numLevels; j++)
+            for (int j = 0; j < numLevels; ++j)
             {
                 int iterator = (int)(stimLevel * numLevels / stimRange);
                 if (response == 1)
@@ -173,12 +143,7 @@ public class RotationTest : Redirector
             //record response and write to file
             //1 if correct and -1 if incorrect
             currentGain = stimLevel;
-            ApplyRedirection();
-
         }
-
-        
-
     }
 
 
